58362ea0a72e687a517b5c270dda07770f78ee82
4x3l15 <adramirez@escuelasproa.edu.ar>
2024-02-17 19:58:13 -0300

    get all users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
 
 const userRouter = require("./routes/userRoutes.js");
 const port = 9500;
 
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use('/api', userRouter);
-app.set('view engine', 'ejs');
-//routes
-app.get("/buscar",(req,res) => {
- res.render("index.ejs");
-});
+
 
 //connection de mongodb
 mongoose.connect("mongodb+srv://adramirez:axel__15@cluster0.ncj4zui.mongodb.net/?retryWrites=true&w=majority")
@@ -23,3 +19,6 @@ mongoose.connect("mongodb+srv://adramirez:axel__15@cluster0.ncj4zui.mongodb.net/
 .catch((error) => console.log(error));
 
 app.listen(port,() => console.log("el sever esta escuchando",port));
+app.get(port,(req,res) =>{
+     res.console.log("el sever esta escuchando")
+ });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const User = require("../models/user");
+const userSchema = require("../models/user");
 
 const router = express.Router();
 
 // Ruta para crear un nuevo usuario
 router.post('/users', (req, res) => {
   // Crear una nueva instancia de usuario utilizando el modelo User
-  const user = new User(req.body);
+  const user = new userSchema(req.body); // Debes usar el modelo User, no userSchema
   // Guardar el usuario en la base de datos
   user.save()
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error.message }));
 });
 
+// Obtener usuarios
+router.get('/users', (req, res) => {
+  userSchema.find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error.message }));
+});
+
 module.exports = router;
+
